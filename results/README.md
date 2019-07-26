194081655eec533ced75c76dd917dc18e1860add
fr1t2 <fr1t2@protonmail.com>
2019-07-25 19:12:54 -0600

    Moving results to dir, and attempting to add product details to file

---
+++ results/README.md
@@ -0,0 +1,6 @@
+### Results!
+
+
+This directory will contain all results found by the system.
+
+The documents here will be overwritten as new queries are run for diferent parts
--- test.py
+++ test.py
@@ -5,15 +5,17 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import time
 
-
+# query the productList.txt for what to search. Will run this for each found
 with open("productList.txt") as p:
+    #Create an array for the stuffs
     productNum = []
+    # For loop below opens and reads all lines in the file, and adds them to the array
     for item in map(str.strip, p):
-        print (item)
+        #print (item)
         with open("storesall.txt") as s:
             data = []
             for store in map(str.strip, s):
-                print (store)
+               # print (store)
                 url = "https://www.lowes.com/pd/search/{}/pricing/{}".format(item, store)
                 response = requests.get(url, timeout=None)
                 soup = BeautifulSoup(response.content, "html.parser")
@@ -31,19 +33,19 @@ with open("productList.txt") as p:
                 else:
                    print(store, " Not Found...")
         #this saves the results once the loop is done to results.csv
-        with open('{}_results.csv'.format(item), 'w') as outfile:
+        with open('results/{}_results.csv'.format(item), 'w') as outfile:
             f = csv.DictWriter(outfile, ['item', 'store', 'price'],
                             delimiter=',', lineterminator='\n')
             f.writeheader()
-            f.writerows(data)
+            f.writerows(data, productNum) # may be an error, and need to remove the productNum here
         #This takes the results and:
         #1)adds store info (city, state, zip, etc)
         #2)removes any blank lines (lines that dont have a price)
         #3)sorts by price (low to high)
         #4)output is saved as LowesResultsAll.csv, overwriting any existing file.
-        first = pd.read_csv('{}_results.csv'.format(item))
+        first = pd.read_csv('results/{}_results.csv'.format(item))
         second = pd.read_csv('allstores.csv')
         first = first[pd.notnull(first['price'])]
         first.sort_values(["price"], inplace=True, ascending=True)  
         merged = pd.merge(first, second, how='left', on='store')
-        merged.to_csv('{}_LowesResultsAll.csv'.format(item), index=False)
+        merged.to_csv('results/{}_LowesResultsAll.csv'.format(item), index=False)
